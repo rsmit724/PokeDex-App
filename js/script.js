109c478f3d3102505e789789d248606edf232687
rsmit724 <138529566+rsmit724@users.noreply.github.com>
2023-08-11 13:30:09 -0700

    added object 'charmander' to the pokemonList array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let pokemonList = [
     {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
     {name: 'Pikachu', height: 4, type: 'electric'},
+    {name: 'Charmander', height: 5, type: 'fire'},
 ];
