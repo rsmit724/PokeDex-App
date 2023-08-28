976eecc088b95c0f13d97b8699348d153552e184
rsmit724 <138529566+rsmit724@users.noreply.github.com>
2023-08-28 14:38:53 -0700

    fixed an error in the link to the css for the application

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
-    {name: 'Pikachu', height: 4, type: 'electric'},
-    {name: 'Charmander', height: 5, type: 'fire'},
+    {
+    name: 'Bulbasaur', 
+    height: 11, 
+    type: ['grass', ' poison']
+    },
+    {
+    name: 'Pikachu',
+    height: 4, 
+    type: 'electric'
+    },
+    {
+    name: 'Charmander',
+    height: 6, 
+    type: 'fire'
+    },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`${pokemonList[i].name} <br> (height: ${pokemonList[i].height}) <br> (type: ${pokemonList[i].type})`);
+    if (pokemonList[i].height > 10) {
+        document.write('<p>' + "This is a large Pokemon!" + "</p>");
+    }else if (pokemonList[i].height < 10 && pokemonList[i].height > 5){
+        document.write('<p>' + "This is an average Pokemon!" + "</p>");
+    }else {
+        document.write('<p>' + "This is a small Pokemon!" + "</p>");
+    }
+}
