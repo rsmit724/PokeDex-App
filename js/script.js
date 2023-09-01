299c447ec6c7e21d42157716957a7f075be8e13c
rsmit724 <138529566+rsmit724@users.noreply.github.com>
2023-09-01 14:48:01 -0700

    commented out sections of older code
    will delete entirely soon, using them as a reference for the time being

--- js/script.js
+++ js/script.js
@@ -46,6 +46,28 @@ pokemonRepository.getAll().forEach(function(listItem) {
             document.write('<p>' + "This is a small Pokemon!" + "</p>");
         }
 });
+
+// pokemonRepository.getAll().forEach(function(listItem) {
+//     let unorderedList = document.querySelector('.pokemon-list');
+//     let unorderedListItem = document.createElement('li');
+//     let button = document.createElement('button');
+//         button.innerText = listItem.name;
+//         button.classList.add('pokemon-button');
+//     unorderedListItem.appendChild(button);
+//     unorderedList.appendChild(unorderedListItem);
+// });
+
+// pokemonRepository.getAll().forEach(function(listItem) {
+//     document.write(`${listItem.name} <br> (height: ${listItem.height}) <br> (type: ${listItem.type}) <br>`);
+//         if (listItem.height > 10) {
+//                 document.write('<p>' + "This is a large Pokemon!" + "</p>");
+//             }else if (listItem.height < 10 && listItem.height > 5){
+//                 document.write('<p>' + "This is an average Pokemon!" + "</p>");
+//         }else {
+//             document.write('<p>' + "This is a small Pokemon!" + "</p>");
+//         }
+// });
+
 // for (let i = 0; i < pokemonList.length; i++) {
 //     document.write(`${pokemonList[i].name} <br> (height: ${pokemonList[i].height}) <br> (type: ${pokemonList[i].type})`);
 //     if (pokemonList[i].height > 10) {
