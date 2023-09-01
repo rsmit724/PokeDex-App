d16a0b3da65a15ebeeeb90a6d4d507f7717d5987
rsmit724 <138529566+rsmit724@users.noreply.github.com>
2023-08-31 17:48:09 -0700

    commented out previous for loop

--- js/script.js
+++ js/script.js
@@ -26,3 +26,13 @@ for (let i = 0; i < pokemonList.length; i++) {
         document.write('<p>' + "This is a small Pokemon!" + "</p>");
     }
 }    
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(`${pokemonList[i].name} <br> (height: ${pokemonList[i].height}) <br> (type: ${pokemonList[i].type})`);
+//     if (pokemonList[i].height > 10) {
+//         document.write('<p>' + "This is a large Pokemon!" + "</p>");
+//     }else if (pokemonList[i].height < 10 && pokemonList[i].height > 5){
+//         document.write('<p>' + "This is an average Pokemon!" + "</p>");
+//     }else {
+//         document.write('<p>' + "This is a small Pokemon!" + "</p>");
+//     }
+// }
