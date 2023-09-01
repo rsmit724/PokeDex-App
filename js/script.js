3c40c5e112b85ded4f34185d8db3d2566f26d7b0
rsmit724 <138529566+rsmit724@users.noreply.github.com>
2023-09-01 14:52:51 -0700

    replaced previous forEach loop code with a more streamlined version
    this works in conjunction with the addListItem function within the pokemonRepository IIFE

--- js/script.js
+++ js/script.js
@@ -48,16 +48,9 @@ let pokemon = {name: 'Charizard', height: 25, type: 'fire'};
 
 pokemonRepository.getAll();
 pokemonRepository.add(pokemon);
-    
-pokemonRepository.getAll().forEach(function(listItem) {
-    document.write(`${listItem.name} <br> (height: ${listItem.height}) <br> (type: ${listItem.type}) <br>`);
-        if (listItem.height > 10) {
-                document.write('<p>' + "This is a large Pokemon!" + "</p>");
-            }else if (listItem.height < 10 && listItem.height > 5){
-                document.write('<p>' + "This is an average Pokemon!" + "</p>");
-        }else {
-            document.write('<p>' + "This is a small Pokemon!" + "</p>");
-        }
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
 
 // pokemonRepository.getAll().forEach(function(listItem) {
