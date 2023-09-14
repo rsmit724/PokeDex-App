997c1077766ba4da5771bbd71779fafebeaa245f
rsmit724 <138529566+rsmit724@users.noreply.github.com>
2023-09-14 16:40:20 -0700

    added comments for readability

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+//IIFE for generating and displaying pokemonList
 let pokemonRepository = (function() {
+    //empty array for pokemonList
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+    //function to return retrieved data to the pokemonList array
     function getAll() {
         return pokemonList;
     }
 
+    //add pokemon to the pokemonList only if the item is an object and includes a name
     function add(pokemon) {
         if (
             typeof pokemon === 'object' &&
@@ -17,6 +21,7 @@ let pokemonRepository = (function() {
         }
     }
 
+    //add pokemon to the UL in the index body, with each list item being displayed as a button
     function addListItem(pokemon) {
         let unorderedList = document.querySelector('.pokemon-list');
         let unorderedListItem = document.createElement('li');
@@ -28,14 +33,17 @@ let pokemonRepository = (function() {
 
         button.addEventListener('click', function(event) {
             showDetails(pokemon);
+        //calls the showDetails function when a button in the UL is clicked
         })
     };
 
+    //retrieve data from the apiURL and return in json format
     function loadList() {
         return fetch(apiUrl).then(function (response) {
           return response.json();
         }).then(function (json) {
           json.results.forEach(function (item) {
+            //each pokemon will be an object that includes the name and link to more details about the pokemon
             let pokemon = {
               name: item.name,
               detailsUrl: item.url
@@ -48,7 +56,7 @@ let pokemonRepository = (function() {
         })
       }
     
-
+    //load specific details (img, height, and types) from the detailsUrl of each pokemon added to the list
     function loadDetails(item) {
       let url = item.detailsUrl;
         return fetch(url).then(function (response) {
@@ -56,6 +64,7 @@ let pokemonRepository = (function() {
     }).then(function (details) {
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
+        //modify this later on to include pokemon types in the modal
         item.types = details.types;
       }).catch(function (e) {
         console.error(e);
@@ -65,9 +74,11 @@ let pokemonRepository = (function() {
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
           console.log(pokemon);
+        //close the modal by clicking outside of it
         });
       }
 
+      //open and close modal using keyboard
     return {
         getAll : getAll,
         add : add,
@@ -77,7 +88,9 @@ let pokemonRepository = (function() {
         showDetails: showDetails,
     };
 })();
+//end of pokemonRepository IIFE
 
+//calling the functions in the pokemonRepository IIFE 
 pokemonRepository.loadList().then(function() {   
     pokemonRepository.getAll().forEach(function(pokemon){
         pokemonRepository.addListItem(pokemon);
